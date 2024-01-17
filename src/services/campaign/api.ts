ecb13dc4515b9a4adeeb51b86d2a164a4ef79360
Le Dinh Quyen <quyenld9699@gmail.com>
2024-01-17 23:00:29 +0700

    feat: add campaign

--- src/services/campaign/api.ts
+++ src/services/campaign/api.ts
@@ -1,12 +1,106 @@
 import axios from 'axios';
+import { apiUrl } from '../url';
+import { BACKEND_BASE_URL } from '../baseUrl';
 
-export type TCampaignData = { name: string; type: string; date: string; capacity: string };
+const getJwt = () => {
+    return localStorage.getItem(LocalStorageKey.AccessToken) || '';
+};
+
+export type TCampaignData = { name: string; type: string; date: string; capacity: string; img?: string; banner?: string; status?: number };
 export const apiLatestFundingCampaigns = '';
 export async function getLatestFundingCampaigns(): Promise<TCampaignData[]> {
-    // const response = await axios.get(apiGetTopProject)
+    const response: any[] = (await axios.get(apiUrl.getCampaign + '?owner=B62qmtfTkHLzmvoKYcTLPeqvuVatnB6wtnXsP6jrEi6i2eUEjcxWauH')).data;
 
-    return [
-        { name: 'Watcher.Guru', date: '2023/12/1', capacity: '0/40 projects', type: 'Public, Grants' },
-        { name: 'Watcher.Guru', date: '2023/12/1', capacity: '0/40 projects', type: 'Public, Grants' },
-    ];
+    return response.map((item) => ({
+        name: item.ipfsData.name || '',
+        type: 'Public, grans',
+        capacity: String(item.ipfsData.capacity || ''),
+        date: new Date().toLocaleDateString(),
+        img: item.ipfsData.img || '',
+        banner: '',
+        status: item.status || 0,
+    }));
+}
+
+export type TCampaignDetailOverview = {
+    organizer: string;
+    capacity: string;
+    description: string;
+    application: {
+        from: string;
+        to: string;
+    };
+    investment: {
+        from: string;
+        to: string;
+    };
+    allocation: {
+        from: string;
+        to: string;
+    };
+};
+export type TCampaignResult = {};
+export type TCampaignDetail = {
+    name: string;
+    banner?: string;
+    overview: TCampaignDetailOverview;
+    result: TCampaignResult;
+};
+export async function getCampaignOverview(campaignId: string): Promise<TCampaignDetail> {
+    const responseOverview: any = (await axios.get(apiUrl.campaignDetail + `/${campaignId}`)).data;
+    // const responseResult: any = (await axios.get(apiUrl.campaignDetail + `/${campaignId}/result`)).data;
+    return {
+        name: responseOverview.ipfsData.name,
+        banner: '',
+        overview: {
+            organizer: responseOverview.owner,
+            capacity: responseOverview.ipfsData.capacity,
+            description: responseOverview.ipfsData.description,
+            allocation: responseOverview.ipfsData?.timeline?.allocation || '',
+            application: responseOverview.ipfsData?.timeline?.application || '',
+            investment: responseOverview.ipfsData?.timeline?.investment || '',
+        },
+        result: {},
+    };
+}
+
+export type CampaignInput = {
+    name: string;
+    coverImage: string;
+    avatarImage: string;
+    description: string;
+    timeline: {
+        participation: {
+            from: string;
+            to: string;
+        };
+        investment: {
+            from: string;
+            to: string;
+        };
+        allocation: {
+            from: string;
+            to: string;
+        };
+    };
+    privacyOption: {
+        isPrivate: true;
+        committeeId: 0;
+        keyId: 0;
+    };
+    capacity: 0;
+    fundingOption: 0;
+    questions: {
+        question: string;
+        hint: string;
+        isRequired: true;
+    }[];
+};
+
+export async function postCampaignToIpf(input: CampaignInput) {
+    const response: {
+        Name: string;
+        Hash: string;
+        Size: number;
+    } = await axios.post(apiUrl.postCampaignToIpfs, input, { headers: { Authorization: `Bearer ${getJwt()}` } });
 }
