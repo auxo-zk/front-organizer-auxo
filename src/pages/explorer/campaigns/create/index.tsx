2ddc33e3fa86a60cbe49e50da637874d2cc51be0
Le Dinh Quyen <quyenld9699@gmail.com>
2024-01-18 11:41:41 +0700

    move create campaign

--- src/pages/explorer/campaigns/create/index.tsx
+++ src/pages/explorer/campaigns/create/index.tsx
@@ -1,7 +1,7 @@
 import dynamic from 'next/dynamic';
 
 // import CreateCampaign from 'src/views/campaign/CreateCampaign';
-const CreateCampaign = dynamic(() => import('src/views/campaign/CreateCampaign'), { ssr: false });
+const CreateCampaign = dynamic(() => import('src/views/explorer/campaign/CreateCampaign/CreateCampaign'), { ssr: false });
 
 export default function CreateCampaignPage() {
     return <CreateCampaign />;
